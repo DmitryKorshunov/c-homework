d95ecbd5a3903a205489ee3fac947446eef50c92
dima korshunov <dimon4ik142@yandex.ru>
2023-01-19 20:07:56 +0300

    Написал программу 2

---
+++ TASK2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.WriteLine("Введите число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1>num2 && num1 > num3)
+            {
+                Console.WriteLine("Большее число " + num1);
+            }else if(num2>num1 && num2>num3)
+            {
+                Console.WriteLine("Большее число " + num2);
+            }
+            else
+            {
+                Console.WriteLine("Большее число " + num3);
+            }
+        }
+    }
+}
