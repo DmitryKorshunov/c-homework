96d2c8b789dbd70403d99357c7e9a5a1f0569ad6
dima korshunov <dimon4ik142@yandex.ru>
2023-01-19 20:31:19 +0300

    Написал программу 3

---
+++ TASK4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+    while (a <= N)
+    {
+        if (a % 2 == 0)
+        {
+         Console.WriteLine(a);
+        }
+        a += 1;
+    }
